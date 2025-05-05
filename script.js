b10c9cd12e75335c0322920ff41990bf95e9dd6b
Oyindamola1111 <ajalajohnson27@gmail.com>
2025-05-05 17:25:44 +0100

    first commit

--- script.js
+++ script.js
@@ -1,39 +1,41 @@
-console.log(500);
-let a,b,c;
-a=5;
-b=6;
-c=a+b;
-console.log(a,b,c);
+// console.log(500);
+// let a,b,c;
+// a=5;
+// b=6;
+// c=a+b;
+// console.log(a,b,c);
 
-let person="hege";
-console.log(person);
+// const { StatementSync } = require("node:sqlite");
+
+// let person="hege";
+// console.log(person);
 
 //addition operators
-let x=20;
-let y=12;
-let z=x+y;
-console.log(x,y,z); 
+// let x=20;
+// let y=12;
+// let z=x+y;
+// console.log(x,y,z); 
 
 //comparism operator
-const d=23;
-const e=15;
-const f=70;
-const g="shade";
-const h="mali";
+// const d=23;
+// const e=15;
+// const f=70;
+// const g="shade";
+// const h="mali";
 //equals to
-console.log(d==e);
+// console.log(d==e);
 
 //greater than
-console.log(d > e);
+// console.log(d > e);
 
 //less than
-console.log(d < e);
+// console.log(d < e);
 
 //greater than equals to
-console.log(d >= e);
+// console.log(d >= e);
 
 //less than equals to
-console.log(d <= e);
+// console.log(d <= e);
 
 // Logical operator
 // THEY ALL RETURN A BULLEAN VALUE 
@@ -42,56 +44,122 @@ console.log(d <= e);
 // logical AND &&
 
 //dami admission
-const fee = false;
-const uniform = false;
-const textbooks = false;
-const idCard = false;
+// const fee = false;
+// const uniform = false;
+// const textbooks = false;
+// const idCard = false;
 
-const checkOR = fee || textbooks || uniform || idCard;
-console.log(checkOR);
+// const checkOR = fee || textbooks || uniform || idCard;
+// console.log(checkOR);
 
-const toy = false;
-const base= false;
-const  spider = false;
-const home = false;
+// const toy = false;
+// const base= false;
+// const  spider = false;
+// const home = false;
 
-const checkAND = toy && base && spider && home;
-console.log(checkAND);
-
-
-const joy = true;
-const bean = true;
-const  rose = true;
-const ball = true;
-
-const checkDami = joy && bean && rose && ball;
-console.log(checkDami);
+// const checkAND = toy && base && spider && home;
+// console.log(checkAND);
 
 
-const poll = true;
-const man = true;
-const  girl = false;
-const game = false;
+// const joy = true;
+// const bean = true;
+// const  rose = true;
+// const ball = true;
 
-const checkGuy = poll || man && girl && game;
-console.log(checkGuy);
+// const checkDami = joy && bean && rose && ball;
+// console.log(checkDami);
+
+
+// const poll = true;
+// const man = true;
+// const  girl = false;
+// const game = false;
+
+// const checkGuy = poll || man && girl && game;
+// console.log(checkGuy);
 
 //URINARY OPERATOR
 //increment ++
 //Decrement --
 
-let k = 3;
-k++;
-console.log(k);
+// let k = 3;
+// k++;
+// console.log(k);
 
-let kolo=6;
-kolo--;
-console.log(kolo)
+// let kolo=6;
+// kolo--;
+// console.log(kolo)
 
 //TENARY OPERATOR
 // (condition) ? "" : ""
-const tenTest =76 > 30 ? "very true" : "not true";
-console.log(tenTest);
+// const tenTest =76 > 30 ? "very true" : "not true";
+// console.log(tenTest);
+
+// CONDITIONALS
+// if Statement
+// if else Statement
+// if else if Statement
+
+// if () {}
+
+// let a = 78;
+// let b = 56;
+// console.log(a>b);
+
+// if (a>b) {
+//     console.log("The first value is the greatest");
+// }
+
+// if else condition
+
+// if (a>b) {
+//     console.log("The first value is the greatest");
+// } else{
+//     console.log("the second value is the greatest");
+// }
+
+// let DBpass = "sam1234";
+// let password = "sam1234";
+
+// if (password === DBpass) {
+//     console.log("logged in successfully");
+// } else {
+//     console.log("incorrect password");
+// }
+
+// PROMPT
+
+// let name = prompt("what is your name");
+// console.log(name);
+
+// const myAge = prompt("enter your Age");
+// console.log(myAge);
+// alert(myAge);
+
+// const test = 78;
+// console.log(`your lucky number is ${test}`);
+
+// const username = prompt("enter your name");
+// alert(`welcome ${username}`);
+
+// let valueOne = prompt("enter your first number");
+// let valueTwo = prompt("enter your second number");
+// let ans = valueOne + valueTwo;
+// alert(ans);
+
+// let valueOne = parseInt(prompt("enter your first number"));
+// let valueTwo = parseInt(prompt("enter your second number"));
+// let ans = valueOne + valueTwo;
+// alert(ans);
+
+// write a programe that checks if a number is even or odd
+
+const myNum = parseInt(prompt("enter any number"));
+if(myNum % 2 === 0){
+    alert(`${myNum} this is an even number`)
+} else{
+    alert(`${myNum} this is an odd number`)
+};
 
 
 
